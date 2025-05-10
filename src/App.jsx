d48e25122c81284231799cbb0d90148d4ce2f1e5
suma poojary <sumalathapoojary1901@gmail.com>
2025-05-10 16:19:31 +0530

    add the api links

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 9876;
+const windowSize = 10;
+let numberWindow = [];
+
+const fetchNumbers = async (type) => {
+    let url;
+    switch (type) {
+        case 'p':
+            url = 'http://20.244.56.144/evaluation-service/primes';
+            break;
+        case 'f': 
+            url = 'http://20.244.56.144/evaluation-service/fibo';
+            break;
+        case 'e': 
+            url = 'http://20.244.56.144/evaluation-service/even';
+            break;
+        case 'r': 
+            url = 'http://20.244.56.144/evaluation-service/rand';
+            break;
+        default: 
+            return { numbers: [] };
+    }
+
+  }
